8e05f1e468b25c8a1213e01a49b48f98c1e2cdf0
LizzieM2003 <liz_mendes2001@yahoo.co.uk>
2016-09-30 06:56:08 +0000

    Create chatCat plugin

--- server.js
+++ server.js
@@ -2,16 +2,13 @@
 
 const express = require('express');
 const app = express();
+const chatCat = require('./app');
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res, next) => {
-	// res.send('<h1>Hello Express!</h1>');
-	// res.sendFile(__dirname + '/views/login.htm');
-	res.render('login');
-});
+app.use('/', chatCat.router);
 
 app.listen(app.get('port'), () => {
 	console.log('ChatCAT running on port:', app.get('port'));
