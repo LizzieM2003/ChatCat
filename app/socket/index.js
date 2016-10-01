1b3a454f9e7086b3be37286807337f17121a8c21
LizzieM2003 <liz_mendes2001@yahoo.co.uk>
2016-10-01 13:10:20 +0000

    Render list of chatrooms

--- app/socket/index.js
+++ app/socket/index.js
@@ -3,7 +3,21 @@
 module.exports = (io, app) => {
 	let allRooms = app.locals.chatrooms;
 
+	allRooms.push({
+		room: 'Good Food',
+		roomID: '0001',
+		users: []
+	});
+
+	allRooms.push({
+		room: 'Good Conversation',
+		roomID: '0002',
+		users: []
+	});
+
 	io.of('/roomslist').on('connection', socket => {
-		console.log('Socket.io connected to client!');
+		socket.on('getChatrooms', () => {
+			socket.emit('chatRoomsList', JSON.stringify(allRooms));
+		});
 	});
 }
