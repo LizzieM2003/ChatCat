f6da9fb23b1f6841e9ae9cc0ab4e99875d14e668
LizzieM2003 <liz_mendes2001@yahoo.co.uk>
2016-10-01 10:29:34 +0000

    Add twitter authentication

--- app/routes/index.js
+++ app/routes/index.js
@@ -18,6 +18,11 @@ module.exports = () => {
 			'/auth/facebook/callback': passport.authenticate('facebook', {
 				successRedirect: '/rooms',
 				failureRedirect: '/'
+			}),
+			'/auth/twitter': passport.authenticate('twitter'),
+			'/auth/twitter/callback': passport.authenticate('twitter', {
+				successRedirect: '/rooms',
+				failureRedirect: '/'
 			})
 			// '/getsession': (req, res, next) => {
 			// 	res.send(`My favourite colour: ${req.session.favColor}`);
